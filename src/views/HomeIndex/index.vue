31707a0741450792ae8e921b85c6bf5279c1ae4d
GodGodJ <2635121825@qq.com>
2021-05-27 16:12:42 +0800

    导航和底部（工作室提交）

---
+++ src/views/HomeIndex/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <nav-bar></nav-bar>
+        <router-view></router-view>
+        <my-footer></my-footer>
+    </div>
+</template>
+
+<style></style>
+
+<script>
+import NavBar from '../../components/nav'
+import MyFooter from '../../components/footer'
+    export default {
+        components:{
+            NavBar,
+            MyFooter
+        }
+    }
+</script>
